643ef427f4ad940eaa695b8543a5c14d5aea37d6
rcodonnell <rcoreyodonnell@gmail.com>
2015-11-04 23:02:58 -0500

    Init commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/style.css' : 'sass/style.scss'
+                },
+                options: {
+                    style: 'expanded',
+                    noCache: true,
+                    sourcemap: 'none'
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: ['sass/**/*.scss'],
+                tasks: ['sass'],
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# Corey and Devin's Wedding Website
+
+This is a website I created to help display information about the wedding and provide easy registry access.
+
+**Live site [here](http://www.coreyanddevin.com).**
+
+## Installation
+
+npm install
+
+## Usage
+
+Run default with `grunt`
+
+`sass` is used to compile the sass file into css.
+The new file is created with the extension `.css`
+
+`watch` to notice a change in the sass file and then executes the `sass` command automatically compiling the file without calling the command yourself.
